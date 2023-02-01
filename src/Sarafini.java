c336a81260b0dd7a60d3918c287b7b7dbb899a61
emiaqn <emiaqn@gmail.com>
2023-02-01 13:17:41 -0600

    meow

--- src/Sarafini.java
+++ src/Sarafini.java
@@ -6,14 +6,32 @@ public class Sarafini {
 
     String w1;
     String w2;
-    Queue<Stack> sarini = new LinkedList<Stack>();
+    Queue<Stack> seeni = new LinkedList<Stack>();
+    List<String> words;
 
-    public Sarafini(String word1, String word2){
+    public Sarafini(String word1, String word2, List<String> w){
         w1 = word1;
         w2 = word2;
+        words = w;
     }
 
-    public
+    public String ladder(){
+
+        seeni.offer(new Stack<String>());
+        seeni.peek().push(w1);
+
+        while(!seeni.isEmpty()){
+            seeni.poll();
+            for(int i = 0; i < words.size(); i++)
+            {
+                int count =0;
+                for(int x =0; x< words.get(i).length(); x++){
+
+                }
+            }
+        }
+
+    }
 
 
 
