ca6e6cfb88c082a75d96f9a9927ffb4665b993e8
emiaqn <emiaqn@gmail.com>
2023-02-02 13:28:03 -0600

    meow

--- src/Sarafini.java
+++ src/Sarafini.java
@@ -8,31 +8,65 @@ public class Sarafini {
     String w2;
     Queue<Stack> seeni = new LinkedList<Stack>();
     List<String> alpha;
+    List<String> dict;
 
-    public Sarafini(String word1, String word2, List<String> a){
+
+
+    public Sarafini(String word1, String word2, List<String> a, List<String> d){
         w1 = word1;
         w2 = word2;
         alpha = a;
+        dict = d;
     }
 
     public String ladder() {
+        boolean sus = false;
+        String answa = "";
 
         seeni.offer(new Stack<String>());
         seeni.peek().push(w1);
 
         while (!seeni.isEmpty()) {
-            seeni.poll();
             for (int j = 0; j < w1.length(); j++) {
                 {
                     for (int i = 0; i < alpha.size(); i++) {
-                        String temp = w1.substring(0, j) + alpha.get(i) + w1.substring(j, w1.length());
+                        String temp = w1.substring(0, j) + alpha.get(i) + w1.substring(j+1);
+                        if(dict.contains(temp))
+                        {
+                            for(Stack<String> s : seeni)
+                            {
+                                for(String teeni : s)
+                                {
+                                    if(teeni.equals(temp))
+                                    {
+                                        sus = true;
+                                    }
+                                }
+                            }
+
+                            if(sus == false) {
+
+                                if (temp.equals(w2)) {
+                                    for (Object lol : seeni.peek()) {
+                                        answa += lol + " ";
+                                    }
+                                    return answa;
+                                }
+                                else{
+                                    Stack<String> teepi = seeni.peek();
+                                    teepi.push(temp);
+                                    seeni.offer(teepi);
+                                }
+                            }
+
+                        }
                     }
 
                 }
             }
 
         }
-        return "lol";
+        return null;
 
 
     }
